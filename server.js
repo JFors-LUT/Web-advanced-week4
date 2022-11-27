75e2c0a3f33cb13aa58199efc0847019a7f7a898
Jarno <jarno.forsblom@student.lut.fi>
2022-11-27 22:21:42 +0200

    deuplicate

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 })*/
 
+app.get("/recipe/:food", (req, res) => {
+    food = {name: req.params.food, 
+            instruction: ["list of strings"],
+            ingredients: ["list of strings"]}
+
+    res.json(food);
+
+})
+
 app.use("/recipe", require("./api/recipe.js"));
 
 app.listen(port, () => console.log(`Server listening a port ${port}!`));
